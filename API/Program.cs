050f7e612db582e36261dc5b7be3f1140dd48308
Szabina Lovas <szabina.lovas.ext@evosoft.com>
2024-01-19 18:24:26 +0100

    change port to 5200

--- API/Program.cs
+++ API/Program.cs
@@ -22,7 +22,7 @@ builder.Services.AddCors(opt =>
 {
     opt.AddPolicy("CorsPolicy", policy =>
     {
-        policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
+        policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3200");
 
     });
 });
